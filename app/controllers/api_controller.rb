5a518a4470ab0c07bcc0a29f06ecf2bf2fba365f
GuiltyMorishita <morishita@lang.cs.tut.ac.jp>
2016-10-01 13:43:37 +0900

    文章をjsonで返せるようにした

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,27 @@
 class ApiController < ApplicationController
-  def index
-    p "hoge"
+  def sentence_generator
+
+    shop_name = params[:shop_name]
+    menu = params[:menu]
+    price = params[:price]
+
+    noodle = Noodle.where(level: 1).limit(1)
+    soup = Soup.where(level: 1).limit(1)
+    pork = Pork.where(level: 1).limit(1)
+
+    youbi = %w[日 月 火 水 木 金 土]
+    date = Date.today.to_era("%O%E年%m月%d日") + youbi[Date.today.wday] + "曜日"
+    sentence = date + "、" + shop_name + "、" + menu + " " + price + "YEN\n"
+    sentence += noodle.first.sentence + "\n"
+    sentence += soup.first.sentence + "\n"
+    sentence += pork.first.sentence
+
+    render :json => sentence
+  end
+
+
+  private
+  def api_params
+    params.permit(:shop_name, :menu, :topping, :price, :noodle_level, :soup_level, :pork_level)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  get "api", to: "api#index"
+  get "api", to: "api#sentence_generator"
 end
